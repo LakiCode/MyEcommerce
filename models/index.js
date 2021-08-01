4b82a65ecf5c85c29718d3238b8c7fe4413c0159
Fule <laki.linux@gmail.com>
2021-07-31 19:20:05 -0700

    import models to a modules/index.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  // where fk is category_id
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  // where fk is ???
+  foreignKey: 'product_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // where fk is product_id
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // where fk is tag_id
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
